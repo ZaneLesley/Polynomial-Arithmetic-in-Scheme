e0ee4ff52684742cabf7646e0be97d2921a850cc
Zane Lesley <zanelesley@ou.edu>
2023-03-30 19:20:51 -0500

    working add/sub

--- homework5.rkt
+++ homework5.rkt
@@ -7,15 +7,22 @@
 
 (define (add a b)
   (letrec ((helper (lambda (a b result)
+
+                     ;base case
                      (if (and (null? a) (null? b))
                          result
+
+                         ;first list empty, add element from second list
                          (if (null? a)
                              (helper a (cdr b) (cons (car b) result))
+                             ;second list empty, add element from first list
                              (if (null? b)
+                                 ;add element from list 1 and 2, combine results
                                  (helper (cdr a) b (cons (car a) result))
                                  (helper (cdr a) (cdr b) (cons (+ (car a) (car b)) result))))))))
     (rev (helper a b '()))))
 
+;Reverses list
 (define (rev l)
 (letrec
     ((helper (lambda (l result)
@@ -24,13 +31,31 @@
   (helper l '())
   ))
 
+
 (define (nestedlist2 list1 list2 func)
   (letrec ((helper (lambda (l1 l2 result)
+                     ;base case, rev if empty
                      (cond ((and (null? l1) (null? l2)) (rev result))
+                           ;if first list empty, add elements from second list
                            ((null? l1) (helper l1 (cdr l2) (cons (car l2) result)))
+                           ;if second list empty, add elements from first list
                            ((null? l2) (helper (cdr l1) l2 (cons (car l1) result)))
-                           ((or (null? (car l1)) (null? (car l2)))
-                            (helper (cdr l1) (cdr l2) result))
+                           ;both list have elements, combine, recurse
                            (else (helper (cdr l1) (cdr l2)
                                          (cons (func (car l1) (car l2)) result)))))))
     (helper list1 list2 '())))
+
+;negates a list
+(define (negate lst)
+  ;base condition, if empty return
+  (cond ((null? lst) '())
+        ;if we still have a list, call function again, but with the sublist
+        ;after recurssion, put them together
+        ((list? (car lst))
+         (cons (negate (car lst))
+               (negate (cdr lst))))
+        (else
+         ;if not a list, multiply element by -1, then reform the sublist
+         (cons (* -1 (car lst))
+               (negate (cdr lst))))))
+
